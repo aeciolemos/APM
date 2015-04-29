7cc884a674443fb838819fff3aa90c421d91424b
Ben Craig <ben.craig@sita.aero>
2015-04-29 22:11:47 +0100

    Initial commit

---
+++ APM.Client/common/productResource.js
@@ -0,0 +1,14 @@
+﻿(function() {
+
+    "use strict";
+
+    function productResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/products/:id");
+    }
+
+    angular.module("common.services")
+        .factory("productResource",
+            ["$resource",
+            "appSettings",
+            productResource]);
+}());
---
+++ APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.OData;
+using APM.WebApi.Models;
+using APM.WebAPI.Models;
+
+namespace APM.WebApi.Controllers
+{
+    [EnableCors("http://localhost:7972", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        [EnableQuery()]
+        public IQueryable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve().AsQueryable();
+        }
+
+        // GET: api/Products/5
+        public Product Get(int id)
+        {
+            Product product;
+            var productRepository = new ProductRepository();
+
+            if (id > 0)
+            {
+                var products = productRepository.Retrieve();
+                product = products.FirstOrDefault(p => p.ProductId == id);
+            }
+            else
+            {
+                product = productRepository.Create();
+            }
+
+            return product;
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]Product product)
+        {
+            var productRepository = new ProductRepository();
+            productRepository.Save(product);
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]Product product)
+        {
+            var productRepository = new ProductRepository();
+            productRepository.Save(id, product);
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
