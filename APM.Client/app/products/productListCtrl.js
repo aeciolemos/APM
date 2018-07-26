40fe77bb0a22b62af409156d423b1570f9a02761
Aecio Lemos <aeciolemos@gmail.com>
2018-07-26 01:25:00 -0300

    - Price shown in Edit form implemented - Currency conversion in product list implemented This completes the backlog for this project. Ready for QA

--- APM.Client/app/products/productListCtrl.js
+++ APM.Client/app/products/productListCtrl.js
@@ -3,12 +3,34 @@
 
     function productListCtrl(productResource) {
         var vm = this;
+        
+        vm.currencySymbol = "$";
 
         vm.searchCriteria = "GDN"; // could be bound to a user input on the front end
 
-        productResource.query(function (data) {
-            vm.products = data;
-        });
+        vm.LoadData = function () {
+            productResource.query(function (data) {
+                vm.products = data;
+            })
+        };
+        vm.LoadData();
+
+        // Convert list items currency
+        vm.convertCurrency = function () {
+            console.log("converting");
+            if (vm.currencySymbol == "$") {
+                angular.forEach(vm.products, function (product, key) {
+                    // Dollars to Euros
+                    product.price = product.price * 0.9;
+                })
+                vm.currencySymbol = "\u20ac"
+            }
+            else {
+                // Euros to Dollars by reloading data
+                vm.LoadData();
+                vm.currencySymbol = "$"
+            };
+        };
     }
 
     angular
